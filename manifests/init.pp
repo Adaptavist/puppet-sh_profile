004f621aedce1a16170eaec86e66b53d0cf5ddb6
Matthew Hope <mhope@adaptavist.com>
2015-12-29 21:39:01 +0000

    lint fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class sh_profile {
     file {
         '/etc/profile.d':
-            source  => "puppet:///modules/${module_name}/",
             ensure  => 'directory',
+            source  => "puppet:///modules/${module_name}/",
             recurse => true,
             purge   => false, # Don't purge this dir, since under RedHat there are several required scripts
             force   => true,
